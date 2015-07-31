3c0f3ff02858f8acfa13e36621d19575f5b4ada4
kuro112 <kuro@xtcr.net>
2015-07-30 17:46:57 -0700

    fixed stoof

--- app/views.py
+++ app/views.py
@@ -138,9 +138,9 @@ def success():
 
 
     if g.user.ptype == 1:
-        html = "<p>Thank you for signing up %s!</p><p>You will be emailed with further details, you can also track our bracket here: http://xtcr.challonge.com/</p><p>You will be receiving a friend request in steam from a user named xTcR | Tournament [IN]. You will need to accept this friend request to be verified on the date of the tournament.</p>"%g.user.nickname
+        html = "<p>You will be receiving a friend request in steam from a user named xTcR | Tournament [IN]. You will need to accept this friend request to be verified on the date of the tournament.</p><p>The tournament will be on August 15th, 2015 at 1:00pm EST. You and your team will need to be in xTcR's teamspeak (ts3.xtcr.net > INS Tournament Waiting Room) 1 hour before the starting time in order to be verified.</p>"
     else:
-        html = "<p>Thank you for signing up %s!</p><p>You will be emailed with further details, you can also track our bracket here: http://xtcr.challonge.com/</p><p>You will be receiving a friend request in steam from a user named xTcR | Tournament [IN]. You will need to accept this friend request to be verified on the date of the tournament.</p>"%g.user.nickname
+        html = "<p>You will be receiving a friend request in steam from a user named xTcR | Tournament [IN]. You will need to accept this friend request to be verified on the date of the tournament.</p><p>The tournament will be on August 15th, 2015 at 1:00pm EST.  You will need to be in teamspeak (ts3.xtcr.net > INS Tournament Waiting Room) 1 and a half hours before the starting time to allow time for teaming up.</p>"
     drill(g.user.email,html)
     output = render_template('success.html',username=g.user)
 
